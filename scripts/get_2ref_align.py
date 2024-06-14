0b2f072088a1bd1c1c41f84e19f082c5759b2ef3
troyeeee <853889815@qq.com>
2024-06-14 12:59:57 +0800

    fix no gene reads

--- scripts/get_2ref_align.py
+++ scripts/get_2ref_align.py
@@ -36,7 +36,7 @@ def build_HLA_ref():
     # Write the reference file
     # shell code
     for gene, alleles in gene_ref_dict.items():
-        if len(alleles)==0:
+        if '-' in alleles[0] or len(alleles)==0:
             continue
         allele_dir=f"{db_build_dir}/{gene}"
         if not os.path.exists(allele_dir):
@@ -58,6 +58,8 @@ def build_HLA_ref():
 
 def map_phased_reads_2_ref():
     for gene, alleles in gene_ref_dict.items():
+        if '-' in alleles[0] or len(alleles)==0:
+            continue
         for allele_idx, allele in enumerate(alleles):
             fq=f"{outdir}/{sample}/{allele}.fq.gz"
             ref=f"{db_build_dir}/{gene}/{gene}.{allele_idx+1}.fasta"
