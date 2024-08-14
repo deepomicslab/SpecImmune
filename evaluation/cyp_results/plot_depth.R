921f8091cfded3703455d4830103d651d8d312e8
wshuai294 <wshuai294@gmail.com>
2024-08-14 12:43:30 +0800

    add all cyp genes

--- evaluation/cyp_results/plot_depth.R
+++ evaluation/cyp_results/plot_depth.R
@@ -1,18 +1,33 @@
 library(ggplot2)
 
-pdf(file="figures/1kg_dp.pdf", width=5, height=3, onefile=FALSE)
-df<-read.table("cyp_depth_cutoff.csv", sep=",", header=TRUE)
 
+# pdf(file="figures/1kg_dp.pdf", width=5, height=3, onefile=FALSE)
+# df<-read.table("cyp_depth_cutoff.csv", sep=",", header=TRUE)
+# ggplot(data=df, aes(x=cutoff, y=accuracy, color = method)) +
+#   geom_line()+
+#   geom_point()+
+#   xlab("No. of reads")+
+#   ylab("Accuracy")+
+#   scale_color_manual(values = c("slateblue1", "tomato"))+theme_classic()
+# dev.off()
 
-
-p <- ggplot(data=df, aes(x=cutoff, y=accuracy, color = method)) +
+pdf(file="figures/hprc_hifi.pdf", width=5, height=3, onefile=FALSE)
+df<-read.table("hprc_hifi_cyp_depth_cutoff.csv", sep=",", header=TRUE)
+ggplot(data=df, aes(x=cutoff, y=accuracy, color = method)) +
   geom_line()+
   geom_point()+
   xlab("No. of reads")+
   ylab("Accuracy")+
   scale_color_manual(values = c("slateblue1", "tomato"))+theme_classic()
-
-# p + scale_fill_manual(values=c("slateblue1" , "tomato"))
-p
-
 dev.off()
+
+pdf(file="figures/hprc_ont.pdf", width=5, height=3, onefile=FALSE)
+df<-read.table("hprc_ont_cyp_depth_cutoff.csv", sep=",", header=TRUE)
+ggplot(data=df, aes(x=cutoff, y=accuracy, color = method)) +
+  geom_line()+
+  geom_point()+
+  xlab("No. of reads")+
+  ylab("Accuracy")+
+  scale_color_manual(values = c("slateblue1", "tomato"))+theme_classic()
+dev.off()
+
