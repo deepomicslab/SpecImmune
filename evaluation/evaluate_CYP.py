eac2d2831b92f9469cb11a4e872bc36734b6a232
wshuai294 <wshuai294@gmail.com>
2024-08-14 14:30:55 +0800

    minor

--- evaluation/evaluate_CYP.py
+++ evaluation/evaluate_CYP.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
     #result_file = "cyp_results/hprc_hifi_cyp_depth_cutoff.csv"
     #main_cyp_hprc(pangu_dir, spec_dir, result_file, 'hprc')
 
-    pangu_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/pangu_hprc_ont/"
-    spec_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/spec_hprc_ont2/"
-    result_file = "cyp_results/hprc_ont_cyp_depth_cutoff.csv"
-    main_cyp_hprc(pangu_dir, spec_dir, result_file, 'hprc')
+    # pangu_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/pangu_hprc_ont/"
+    # spec_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/spec_hprc_ont2/"
+    # result_file = "cyp_results/hprc_ont_cyp_depth_cutoff.csv"
+    # main_cyp_hprc(pangu_dir, spec_dir, result_file, 'hprc')
 
     # pangu_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/pangu_1k/"
     # spec_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/spec_1k3/"
     # result_file = "cyp_results/cyp_depth_cutoff.csv"
     # main_cyp_hprc(pangu_dir, spec_dir, result_file)
 
-    # spec_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/spec_1k3/"
-    # result_file = "cyp_results/cyp_all_gene.csv"
-    # main_all_cyp(spec_dir, result_file)
+    spec_dir = "/home/wangshuai/00.hla/long/experiments/cyp/cyp_results/spec_1k3/"
+    result_file = "cyp_results/cyp_all_gene.csv"
+    main_all_cyp(spec_dir, result_file)
 
     #load_GeT_RM4()
