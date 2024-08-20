83a5e6a42c7553daac1aa58415d6ba676568860b
wshuai294 <wshuai294@gmail.com>
2024-08-20 12:22:55 +0800

    use whole hg38 as ref for vdj

--- scripts/main.py
+++ scripts/main.py
@@ -117,7 +117,7 @@ def main(args):
         my_db = My_db(args)
         command = f"""
         bash {sys.path[0]}/run.phase.IG.TR.sh {args["n"]} {args["r"]} {args["o"]}/{args["n"]} {my_db.full_db} {args["j"]} {args["k"]} {my_db.hg38}
-        python3 {sys.path[0]}/get_IG_TR_depth.py -i {args["i"]} -o {args["o"]} -n {args["n"]} --db {args["db"]} -k {args["k"]} --hg38 {args["hg38"]} -j {args["j"]}
+        python3 {sys.path[0]}/get_IG_TR_depth.py --lite 0 -i {args["i"]} -o {args["o"]} -n {args["n"]} --db {args["db"]} -k {args["k"]} --hg38 {args["hg38"]} -j {args["j"]}
         """
         os.system(command)
     else:
--- scripts/mask_low_depth_region.py
+++ scripts/mask_low_depth_region.py
@@ -13,6 +13,9 @@ import argparse
 from argparse import ArgumentTypeError
 
 
+# IG_TR_chrs = {'chr14:20121838-24045098':1, 'chr14:104363198-108375071':1,'chr2:87360568-91735370':1,'chr22:20530934-24421435':1\
+#               ,'chr7:36753380-39868169':1,'chr7:140799177-144313399':1,'chr2':1,'chr14':1,'chr7':1,'chr22':1}
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -36,6 +39,9 @@ class Mask_low():
         for line in f:
             array = line.strip().split()
             gene = array[0]
+            # if args['i'] == 'IG_TR' and args['r']:
+            #     if gene not in IG_TR_chrs:
+            #         continue
             depth = int(array[2])
             if gene not in self.depth_dict:
                 self.depth_dict[gene] = []
@@ -74,29 +80,33 @@ class Mask_low():
 
     def select_focus_interval(self, depth_list, exon_intervals):
         mask_region = []    
-        if self.focus_exon == True:
+        if args['r']:
             for interval in exon_intervals:
                 mask_region = self.get_low_region(depth_list, mask_region, interval[0], interval[1])
         else: # full length
             mask_region = self.get_low_region(depth_list, mask_region, 1, len(depth_list))
         return mask_region
-
-
-    # def read_exons(self, gene):
-    #     exon_bed = "%s/whole/exon_extent.bed"%(sys.path[0])
-    #     exon_intervals = []
-    #     f = open(exon_bed, 'r')
-    #     for line in f:
-    #         array = line.strip().split()
-    #         if array[0] == gene:
-    #             start = int(array[1])
-    #             end = int(array[2])
-    #             exon_intervals.append([start, end]) 
-    #     f.close()
-    #     return exon_intervals
+    
+    def read_regions(self, gene):
+        """
+        TRAV1-1 chr14 21621838 21622567 +
+        """
+        exon_intervals = []
+        f = open(args['r'], 'r')
+        for line in f:
+            array = line.strip().split()
+            chr = array[1]
+            if chr != gene:
+                continue
+            start = int(array[2])
+            end = int(array[3])
+            exon_intervals.append([start, end]) 
+        f.close()
+        return exon_intervals
 
 
     def main(self):
+        print ("detect low depth regions...")
         self.record_depth()
         # gene = "HLA_DPB1"
         f = open(mask_bed, 'w')
@@ -105,8 +115,10 @@ class Mask_low():
             depth_list = self.depth_dict[gene]
             # mean_depth = np.mean(depth_list[1000:-1000])
             # get 0 ratio in the deoth list
-            zero_ratio = depth_list.count(0)/len(depth_list)
-            print(f"Zero ratio for {gene} is {zero_ratio}", flush=True)
+            # zero_ratio = depth_list.count(0)/len(depth_list)  ## too slow, thus comment this
+            # print(f"Zero ratio for {gene} is {zero_ratio}", flush=True)
+            # print (gene)
+
             depth_list_copy = [x for x in depth_list if x != 0]
 
             mean_depth = np.mean(depth_list_copy)
@@ -114,6 +126,8 @@ class Mask_low():
             mean_depth_dict[gene] = round(mean_depth)
             # exon_intervals = self.read_exons(gene)
             exon_intervals = []
+            if args["r"]:
+                exon_intervals = self.read_regions(gene)
             mask_region = self.select_focus_interval(depth_list, exon_intervals)
             # mask_region = self.get_low_region(depth_list)
             # self.mask_dict[gene] = mask_region
@@ -138,6 +152,8 @@ if __name__ == "__main__":
     optional.add_argument("-w", type=int, help="Windows size while using sliding the ref", metavar="\b", default=20)
     optional.add_argument("-d", type=int, help="Minimum mean depth in a window.", metavar="\b", default=5)
     optional.add_argument("-f", type=str, help="Whether only mask exons.", metavar="\b", default="False")
+    optional.add_argument("-r", type=str, help="only check this region, bed file.", metavar="\b")
+    optional.add_argument("-i", type=str, help="gene class.", metavar="\b", default="HLA")
     optional.add_argument("-b", type=str, help="output bed", metavar="\b")
     optional.add_argument("-h", "--help", action="help")
     args = vars(parser.parse_args()) 
