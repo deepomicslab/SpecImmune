90dc2ee2fc140faa54d2a36e0d18638539f6ac50
wshuai294 <wshuai294@gmail.com>
2024-09-02 11:03:15 +0800

    get 620 results

--- simulation/sim_KIR.sh
+++ simulation/sim_KIR.sh
@@ -2,48 +2,37 @@
 ## use pbsim2
 
 
-outdir=/mnt/d/HLAPro_backup/Nanopore_optimize/data/sim_hap/reads3/
-resultdir=/mnt/d/HLAPro_backup/Nanopore_optimize/data/sim_hap/results3/
+outdir=/mnt/d/HLAPro_backup/Nanopore_optimize/data/sim_hap/reads4/
+resultdir=/mnt/d/HLAPro_backup/Nanopore_optimize/data/sim_hap/results4/
 
 for dp in 5 10 15 20 25 30 35 40
 do
 for i in {1..20}
 do
-    sample=KIR_dp${dp}_acc90_$i
-    # echo $sample
+    sample=KIR_dp${dp}_acc95_$i
+    echo $sample
 
-    result=$resultdir/$sample/$sample.KIR.final.type.result.txt
-    ## check the line number of result, if the line number is 2, then the result is empty
-    if [ -f $result ]; then
-        line_num=$(wc -l $result | awk '{print $1}')
-        if [ $line_num -eq 2 ]; then
-            echo "handle $sample"
-            python3 ../scripts/main.py -n $sample -o $resultdir -j 15 -y pacbio -i KIR -r $outdir/$sample/${sample}.fastq.gz -k 1 --hete_p 0.2
-            # continue
-        fi
-    fi
+    #### simulation
+    mkdir $outdir/$sample
+    perl simu.data.haplotype.kir2.pl $sample $outdir/$sample
+    pbsim --prefix $outdir/$sample/$sample --depth $dp --hmm_model pbsim_model/P4C2.model --accuracy-mean 0.95 $outdir/$sample/$sample.KIR.sep.fa
 
-#     #### simulation
-#     mkdir $outdir/$sample
-#     perl simu.data.haplotype.kir2.pl $sample $outdir/$sample
-#     pbsim --prefix $outdir/$sample/$sample --depth $dp --hmm_model pbsim_model/P4C2.model --accuracy-mean 0.90 $outdir/$sample/$sample.KIR.sep.fa
-
-#     cat $outdir/$sample/${sample}_*fastq>$outdir/$sample/${sample}.fastq
-#     rm $outdir/$sample/${sample}_*fastq
-#     rm $outdir/$sample/${sample}_*.ref
-#     rm $outdir/$sample/${sample}_*.maf
-#     gzip -f $outdir/$sample/${sample}.fastq
+    cat $outdir/$sample/${sample}_*fastq>$outdir/$sample/${sample}.fastq
+    rm $outdir/$sample/${sample}_*fastq
+    rm $outdir/$sample/${sample}_*.ref
+    rm $outdir/$sample/${sample}_*.maf
+    gzip -f $outdir/$sample/${sample}.fastq
 
 
-#     #### run
-#     python3 ../scripts/main.py -n $sample -o $resultdir -j 15 -y pacbio -i KIR -r $outdir/$sample/${sample}.fastq.gz -k 1 --hete_p 0.2
+    #### run
+    python3 ../scripts/main.py -n $sample -o $resultdir -j 15 -y pacbio -i KIR -r $outdir/$sample/${sample}.fastq.gz -k 1 --hete_p 0.2
 
-#     # #### evaluation
-#     # python3 ../evaluation/assess_read_bin.py $resultdir/$sample/${sample}.read_binning.txt $outdir/$sample/$sample.KIR.sep.fa $outdir/$sample/${sample}.fastq.gz 
-#     # python3 ../evaluation/assess_typing.py -i KIR --true $outdir/$sample/$sample.KIR.hap.alleles.txt --infer $resultdir/$sample/${sample}.KIR.type.result.txt 
-#     # echo "----split-----$sample"
-#     # python3 ../evaluation/assess_typing.py -i KIR --true $outdir/$sample/$sample.KIR.hap.alleles.txt --infer $resultdir/$sample/${sample}.KIR.final.type.result.txt 
-#     # break
+    # #### evaluation
+    # python3 ../evaluation/assess_read_bin.py $resultdir/$sample/${sample}.read_binning.txt $outdir/$sample/$sample.KIR.sep.fa $outdir/$sample/${sample}.fastq.gz 
+    # python3 ../evaluation/assess_typing.py -i KIR --true $outdir/$sample/$sample.KIR.hap.alleles.txt --infer $resultdir/$sample/${sample}.KIR.type.result.txt 
+    # echo "----split-----$sample"
+    # python3 ../evaluation/assess_typing.py -i KIR --true $outdir/$sample/$sample.KIR.hap.alleles.txt --infer $resultdir/$sample/${sample}.KIR.final.type.result.txt 
+    # break
 
 done
 done
@@ -52,4 +41,13 @@ done
 # python3 ../scripts/main.py -n $sample -o $resultdir -j 15 -y pacbio -i KIR -r $outdir/$sample/${sample}.fastq.gz -k 1 --hete_p 0.2
 
 
-
+    # result=$resultdir/$sample/$sample.KIR.final.type.result.txt
+    # ## check the line number of result, if the line number is 2, then the result is empty
+    # if [ -f $result ]; then
+    #     line_num=$(wc -l $result | awk '{print $1}')
+    #     if [ $line_num -eq 2 ]; then
+    #         echo "handle $sample"
+    #         python3 ../scripts/main.py -n $sample -o $resultdir -j 15 -y pacbio -i KIR -r $outdir/$sample/${sample}.fastq.gz -k 1 --hete_p 0.2
+    #         # continue
+    #     fi
+    # fi
