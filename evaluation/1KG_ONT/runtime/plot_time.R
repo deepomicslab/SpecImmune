40be758c0c5ccdf5d2d52ec38a674e7a6aa6b33c
wshuai294 <wshuai294@gmail.com>
2024-09-24 14:14:48 +0800

    time plot

--- evaluation/1KG_ONT/runtime/plot_time.R
+++ evaluation/1KG_ONT/runtime/plot_time.R
@@ -1,13 +1,20 @@
 library(ggplot2)
 
 
-pdf(file="time_dot.pdf", width=10, height=8, onefile=FALSE)
+pdf(file="time_dot.pdf", width=5, height=4, onefile=FALSE)
 df<-read.table("vdj_time.csv", sep=",", header=TRUE)
 
 # ggplot(df, aes(x=EUR, y=EAS)) + geom_point()
 # Change the point size, and shape
-ggplot(df, aes(x=EUR, y=EAS, color=locus)) +
-  geom_point(size=1, shape=1)+ 
-  geom_text(label=df$Allele,size=2)
+ggplot(df, aes(x=time, y=mem)) +
+  geom_point()+
+  xlab("CPU Time (h)") +
+  ylab("Peak Memory (GB)")+
+  ggtitle("IG+TCR")
+  # + 
+  # geom_text(label=df$Allele,size=2)
+## count mean value of time and mem
+print(paste("mean time: ", mean(df$time)))
+print(paste("mean mem: ", mean(df$mem)))
 
 dev.off()
