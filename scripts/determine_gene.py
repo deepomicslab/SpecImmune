956273a5a13b0c6f85bf234ab1acd7d5b06a8205
wshuai294 <wshuai294@gmail.com>
2024-11-05 14:57:56 +0800

    support other cyp gene

--- scripts/determine_gene.py
+++ scripts/determine_gene.py
@@ -21,9 +21,9 @@ def get_focus_gene(input):
     elif gene_class == "CYP":
         # gene_list = [ 'CYP19A1', 'CYP1A1', 'CYP1B1', 'CYP26A1', 'CYP2A13', 'CYP2A6', 'CYP2B6', 'CYP2C19', 'CYP2C8', 'CYP2C9', 'CYP2D6', 'CYP2F1', 'CYP2J2', 'CYP2R1', 'CYP2S1', 'CYP2W1', 'CYP3A4', 'CYP3A43', 'CYP4A22', 'CYP4B1', 'CYP4F2', 'CYP8A1', 'CYP3A5', 'CYP3A7' ]
     
-        # gene_list = ['CYP2D6', 'CYP2A13', 'CYP2A6', 'CYP2B6', 'CYP2C19', 'CYP2C8', 'CYP2C9', 'CYP3A4', 'CYP3A5', 'CYP4F2', 'NAT2', 'NUDT15', 'SLCO1B1']
+        gene_list = ['CYP2D6', 'CYP2A13', 'CYP2A6', 'CYP2B6', 'CYP2C19', 'CYP2C8', 'CYP2C9', 'CYP3A4', 'CYP3A5', 'CYP4F2', 'NAT2', 'NUDT15', 'SLCO1B1']
         # print (gene_list)
-        gene_list = ['CYP2D6']
+        # gene_list = ['CYP2D6']
     elif gene_class == "KIR":
         gene_list = ['KIR2DL1', 'KIR2DL2', 'KIR2DL3', 'KIR2DL4', 'KIR2DL5A', 'KIR2DL5B', 'KIR2DP1', 'KIR2DS1', 'KIR2DS2', 'KIR2DS3', 'KIR2DS4', 'KIR2DS5', 'KIR3DL1', 'KIR3DL2', 'KIR3DL3', 'KIR3DP1', 'KIR3DS1']
         # gene_list = ['KIR3DL2']
@@ -101,7 +101,7 @@ def get_folder_list(folder):
     if 'CYP2D6' in folder_list:
         folder_list.remove('CYP2D6')
         folder_list.insert(0, 'CYP2D6')
-        return ['CYP2D6']
+        # return ['CYP2D6']
     # return ['MICA']
     # if 'KIR3DL2' in folder_list:
     #     return ['KIR3DL2']
