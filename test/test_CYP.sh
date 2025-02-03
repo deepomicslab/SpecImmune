04c1a7438ff53cd2719933e7952d5a4a64b1d4ab
troyeeee <853889815@qq.com>
2025-02-03 21:25:16 +0800

    update cyp test

--- test/test_CYP.sh
+++ test/test_CYP.sh
@@ -1,4 +1,5 @@
 db=../db/
+hg38=/path-to-hg38/hg38.fa
 #### please change the db path if your db is not in the default path
 
 python ../scripts/main.py -r CYP/HG03579.CYP.fastq.0.1.fq.gz -j 15 -i CYP -n test_CYP -o test -y pacbio --db $db --hg38 $hg38
