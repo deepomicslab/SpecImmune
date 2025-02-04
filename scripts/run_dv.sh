86f05c1473bdd67791ad6494f6e5c78d708ead34
troyeeee <853889815@qq.com>
2025-02-04 18:17:48 +0800

    add deepvariant param

--- scripts/run_dv.sh
+++ scripts/run_dv.sh
@@ -57,5 +57,5 @@ singularity run -B /usr/lib/locale/:/usr/lib/locale/ \
   --intermediate_results_dir /out_dir/intermediate_results_dir \
   --num_shards=$threads \
   --sample_name='SAMPLE' \
-  --allow_empty_examples
+  --make_examples_extra_args="--allow_empty_examples=true"
 
